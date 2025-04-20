1fd5fc7280d513cc9d3e0ad4834407e2bb10adc1
Your Name <jamestipping@protonmail.com>
2025-04-20 17:37:26 +0100

    feat: Make chess pieces draggable by default

--- src/pieces/ChessPiece.ts
+++ src/pieces/ChessPiece.ts
@@ -3,7 +3,6 @@ import { html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { PieceIconFactory } from './PieceIconFactory';
 import { ChessPieceType } from '../types/ChessBoardElementTypes';
-import { ChessPieceDragStartEvent } from '../types/EventTypes';
 import { Square } from 'chess.js';
 
 @customElement('chess-piece')
@@ -12,46 +11,21 @@ export class ChessPiece extends LitElement {
 
   @property({ type: String }) squareId: Square | undefined;
 
-  dragStart(e: DragEvent) {
-    const preventDrag = () => e.preventDefault();
-    if (this.squareId == null) {
-      console.error('squareId must be defined in ChessPiece');
-      preventDrag();
-      return;
-    }
-    this.dispatchEvent(
-      new ChessPieceDragStartEvent({
-        detail: {
-          preventDrag,
-          squareId: this.squareId,
-        },
-        bubbles: true,
-        composed: true,
-      }),
-    );
+  connectedCallback() {
+    /* eslint-disable-next-line wc/guard-super-call */
+    super.connectedCallback();
+    this.setAttribute("draggable", "true");
+  }
 
-    if (e.dataTransfer) {
-      console.log('dragStart data:', this.squareId);
-      e.dataTransfer.setData('text/plain', this.squareId);
-
-      // Create a clone of the piece element for the drag image
-      const pieceElement = e.target as HTMLElement;
-      const clone = pieceElement.cloneNode(true) as HTMLElement;
-      clone.style.position = 'absolute';
-      clone.style.top = '-1000px';
-      document.body.appendChild(clone);
-
-      // Set the drag image and clean up the clone
-      e.dataTransfer.setDragImage(clone, 22, 22);
-      requestAnimationFrame(() => {
-        document.body.removeChild(clone);
-      });
-    }
+  diconnectedCallback() {
+    /* eslint-disable-next-line wc/guard-super-call */
+    super.disconnectedCallback();
+    this.removeAttribute("draggable");
   }
 
   render() {
     return html`
-      <div @dragstart=${this.dragStart} draggable="true">
+      <div>
         ${PieceIconFactory.createPiece(this.piece)}
       </div>
     `;
